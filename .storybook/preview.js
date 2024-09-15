855a1b72035b838b20704cbe6dce21df3d16b7e6
yukimura1228 <okada091228@gmail.com>
2024-09-15 21:44:06 +0900

    :sparkles: 1. Simple component https://storybook.js.org/tutorials/intro-to-storybook/react/en/simple-component/

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import AXE_LOCALE_JA from "axe-core/locales/ja.json";
+
+import '../src/index.css';
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -5,6 +9,11 @@ const preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
+      },
+    },
+    a11y: {
+      config: {
+        locale: AXE_LOCALE_JA, // Japanese locale
       },
     },
   },
